11cb836d47d688c90378bad281000625e3d5d9c7
kaminenileela <kaminenileela1992@email.com>
2024-08-21 11:45:07 -0400

    expense

--- cicd/main.tf
+++ cicd/main.tf
@@ -27,6 +27,32 @@ module "jenkins_agent" {
   }
 }
 
+resource "aws_key_pair" "tools" {
+  key_name   = "tools"
+  # public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB+fcmTgj3LtZ9uRhv70aoWwgZ1X2zb2vxkworAMKKaQ Leela@LEELA-DESKTOP"
+  public_key = file ("~/.ssh/tools.pub")
+}
+
+module "nexus" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "nexus"
+  key_name = aws_key_pair.tools.key_name
+  instance_type          = "t3.medium"
+  vpc_security_group_ids = [sg-015c3232180617394]
+  subnet_id = "subnet-0f4f0cef3b00114cb"
+  ami = data.aws_ami.nexus_ami_info.id
+  root_block_device = [
+    {
+      volume_type = "gp3"
+      volume_size = 30
+    }
+  ]
+  tags = {
+    Name = "nexus"
+  }
+}
+
 module "records" {
   source  = "terraform-aws-modules/route53/aws//modules/records"
   version = "~> 2.0"
@@ -49,6 +75,15 @@ module "records" {
       ttl     = 1
       records = [
         module.jenkins_agent.private_ip
+      ]
+      allow_overwrite = true
+    },
+    {
+      name    = "nexus"
+      type    = "A"
+      ttl     = 1
+      records = [
+        module.nexus.private_ip
       ]
       allow_overwrite = true
     }
